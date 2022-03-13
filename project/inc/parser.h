0dc61eb39fe1976676fb9980ea7fb217ff5768f0
Mariia <masya.kim@mail.ru>
2022-03-13 20:10:52 +0300

    fix seagfault

--- project/inc/parser.h
+++ project/inc/parser.h
@@ -6,7 +6,7 @@
 /*   By: a79856 <a79856@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 17:46:12 by a79856            #+#    #+#             */
-/*   Updated: 2022/03/01 20:32:53 by a79856           ###   ########.fr       */
+/*   Updated: 2022/03/13 19:57:11 by a79856           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ char	*ft_quotechar(char *str, int *i, t_parser *prs);
 char	*ft_dollar(char *str, int *i, char *start, char *end, t_parser *prs);
 char	*ft_replace(char *str, int *i, char c, t_parser *prs);
 char	*ft_charjoin(char *str, char c);
+void	ft_parse_split(t_parser *prs);
 
 #endif
