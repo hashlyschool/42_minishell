a2e83208538ff120ef50e8210a5c5f95b337c11f
sstyx-ubuntu <sstyx-ubuntu@email>
2022-04-27 00:18:59 +0000

    fix norme error in replase file

--- project/inc/parser.h
+++ project/inc/parser.h
@@ -6,7 +6,7 @@
 /*   By: sstyx <sstyx@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 17:46:12 by a79856            #+#    #+#             */
-/*   Updated: 2022/04/26 23:15:59 by sstyx            ###   ########.fr       */
+/*   Updated: 2022/04/27 00:16:34 by sstyx            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ typedef struct main
 	char	*new_str;
 	char	*new_str2;
 	int		len;
-} t_main;
+}	t_main;
 
 typedef struct s_lex_q
 {
@@ -57,8 +57,7 @@ typedef struct s_lex_q
 	int		count_char;
 	int		ops;
 	int		spase;
-} t_lex_q;
-
+}	t_lex_q;
 
 typedef struct s_preparse
 {
@@ -72,12 +71,21 @@ typedef struct s_preparse
 
 typedef struct t_replase
 {
-	int		j;
-	int		flag;
-	char	*tmp;
-	char	c;
+	int			j;
+	int			flag;
+	char		*tmp;
+	char		c;
+	t_parser	*p;
 }	t_replase;
 
+typedef struct t_replase_utills
+{
+	char	*tmp;
+	int		j;
+	char	*str2;
+	int		len;
+	int		len_prs;
+}	t_rep_utils;
 
 char	*ft_gap(char *str, int *i, t_parser *prs);
 char	*ft_slash(char *str, int *i, t_parser *prs);
@@ -89,6 +97,8 @@ void	ft_parse_split(t_parser *prs);
 char	**ft_minishell_split(char const *s);
 char	*ft_queote_dollar(char *str, char *new_str, int i);
 char	*ft_charjoin_no_free(char *str, char c);
+char	*ft_replace_util(char *str, int *i, char *start, t_replase *t);
+int		ft_is_fd(char *str, int i);
 
 //lexer//
 char	*lexer_init(char *str);
--- project/src/parsing_replase.c
+++ project/src/parsing_replase.c
@@ -6,12 +6,13 @@
 /*   By: sstyx <sstyx@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 03:08:16 by a79856            #+#    #+#             */
-/*   Updated: 2022/04/26 23:20:34 by sstyx            ###   ########.fr       */
+/*   Updated: 2022/04/27 00:17:28 by sstyx            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+//выдать ошибку в 31 строчке
 int	ft_is_fd(char *str, int i)
 {
 	int		a;
@@ -28,81 +29,30 @@ int	ft_is_fd(char *str, int i)
 	a = ft_atoi(num);
 	free(num);
 	if (a > MAX_FD)
-		;//выдать ошибку
+		;
 	if (str[i] == ' ' && a >= 0)
 		return (i + 1);
 	return (-1);
 }
 
-/* продолжение функции для замены & > >> | */
-char	*ft_replace_util(char *str, int *i, int flag, char *start, t_parser *prs)
-{
-	char	*tmp;
-	int		j;
-	char	*str2;
-	int		len;
-	int		len_prs;
-
-	j = *i;
-	tmp = ft_substr(str, 0, *i);
-	str2 = str;
-	tmp = ft_strjoin_free_s1(tmp, start);
-	len_prs = 0;
-	if (str[*i + flag] != '\0')
-		tmp = ft_strjoin_free_all(tmp, ft_strdup(str + *i + flag));
-	if (str[(*i)] == '>')
-	{
-		if (prs->r == 1)
-			prs->r = 0;
-		else
-			prs->r = 1;
-	}
-	len = ft_is_fd(str, (*i) - 1);
-	if (len != (*i) && len != -1)
-	{
-		len_prs = ft_strlen(prs->str);
-		free(prs->str);
-		prs->str = ft_substr(str, len - len_prs + 1, len - (len - len_prs + 1));
-		ft_parse_split(prs);
-		prs->str = ft_substr(str, len, (*i) - len);
-	}
-	if (prs->r == 0 || len == (*i) || len == -1)
-		ft_parse_split(prs);
-	if (str[(*i)] != '>' && str[(*i)] != '<' && str[(*i)] != '*')
-	{
-		if (prs->str && prs->str[0])
-			ft_parse_split(prs);
-		free(prs->str);
-		prs->str = ft_strdup(start);
-		ft_parse_split(prs);
-		free(prs->str);
-		prs->str = NULL;
-	}
-	else
-		prs->str = ft_strjoin_free_s1(prs->str, start);
-	*i += ft_strlen(start) - 1;
-	free(str);
-	return (tmp);
-}
-
-void	ft_replase_continue(char *str, int *i, t_replase *t, t_parser *prs)
+void	ft_replase_continue(char *str, int *i, t_replase *t)
 {
 	if (t->c == '<' && t->flag == 0)
-		t->tmp = ft_replace_util(str, i, t->flag + 1, REDIR_LEFT_ONE, prs);
+		t->tmp = ft_replace_util(str, i, REDIR_LEFT_ONE, t);
 	else if (t->c == '&' && t->flag == 1)
-		t->tmp = ft_replace_util(str, i, t->flag + 1, AND_STR, prs);
+		t->tmp = ft_replace_util(str, i, AND_STR, t);
 	else if (t->c == '|' && t->flag == 1)
-		t->tmp = ft_replace_util(str, i, t->flag + 1, OR_STR, prs);
+		t->tmp = ft_replace_util(str, i, OR_STR, t);
 	else if (t->c == '|' && t->flag == 0)
-		t->tmp = ft_replace_util(str, i, t->flag + 1, PIPE_STR, prs);
+		t->tmp = ft_replace_util(str, i, PIPE_STR, t);
 	else if (t->c == ';' && t->flag == 0)
-		t->tmp = ft_replace_util(str, i, t->flag + 1, SEMICOLON, prs);
+		t->tmp = ft_replace_util(str, i, SEMICOLON, t);
 	else if (t->c == '*' && t->flag == 0)
-		t->tmp = ft_replace_util(str, i, t->flag + 1, STAR, prs);
+		t->tmp = ft_replace_util(str, i, STAR, t);
 	else if (t->c == '(' && t->flag == 0)
-		t->tmp = ft_replace_util(str, i, t->flag + 1, BR_LEFT, prs);
+		t->tmp = ft_replace_util(str, i, BR_LEFT, t);
 	else if (t->c == ')' && t->flag == 0)
-		t->tmp = ft_replace_util(str, i, t->flag + 1, BR_RIGHT, prs);
+		t->tmp = ft_replace_util(str, i, BR_RIGHT, t);
 }
 
 /* функции для замены & > >> | */
@@ -114,17 +64,18 @@ char	*ft_replace(char *str, int *i, char c, t_parser *prs)
 	t.flag = 0;
 	t.tmp = NULL;
 	t.c = c;
+	t.p = prs;
 	prs->red = c;
 	if (str[(*i + 1)] == c && c != ';' && c != '(' && c != ')' && c != '*')
 		t.flag = 1;
 	if (c == '>' && t.flag == 1)
-		t.tmp = ft_replace_util(str, i, t.flag + 1, REDIR_RIGHT_TWO, prs);
+		t.tmp = ft_replace_util(str, i, REDIR_RIGHT_TWO, &t);
 	else if (c == '>' && t.flag == 0)
-		t.tmp = ft_replace_util(str, i, t.flag + 1, REDIR_RIGHT_ONE, prs);
+		t.tmp = ft_replace_util(str, i, REDIR_RIGHT_ONE, &t);
 	else if (c == '<' && t.flag == 1)
-		t.tmp = ft_replace_util(str, i, t.flag + 1, REDIR_LEFT_TWO, prs);
+		t.tmp = ft_replace_util(str, i, REDIR_LEFT_TWO, &t);
 	else if (ft_strchr("<&|;*()", c))
-		ft_replase_continue(str, i, &t, prs);
+		ft_replase_continue(str, i, &t);
 	else
 	{
 		prs->str = ft_charjoin(prs->str, c);
